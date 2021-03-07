587de5d222ecef7aa3b069d3069bb502bfd9234e
Rosari Harlan <rosari.harlan@gmail.com>
2021-03-07 14:19:10 +0100

    Added time zone calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,23 +16,54 @@ const sunrise = document.querySelector("#sunrise .value");
 const sunset = document.querySelector("#sunset .value");
 const geoCoords = document.querySelector("#geoCoords .value");
 
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let city = document.querySelector("#searchField");
   city = city.value;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78c29c57fa1b0939a568dac3a843325c`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78c29c57fa1b0939a568dac3a843325c&units=metric`
   )
     .then((response) => response.json())
     .then((data) => {
       // ---------- main Section ----------------
       cityName.innerHTML = data.name;
       mainIcon.innerHTML = "data.weather[0].icon + hier sollte ein Icon sein";
-      temperature.innerHTML = data.main.temp + "in Celsius umrechnen?";
+      temperature.innerHTML = `${data.main.temp}&#186 C`;
 
       // ---------- detail Section ----------------
-      localTime.innerHTML =
-        "Welchen Datensatz müssen wir hier nehmen? ---- Uhrzeit umrechnen";
+
+      let convertTime = () => {
+        let localDate = new Date();
+        let utc = localDate.getTime() + localDate.getTimezoneOffset() * 60000;
+        let showLocalTime = new Date(utc + 3600000 * (data.timezone / 3600));
+        let localHours =
+          showLocalTime.getHours() < 10
+            ? "0" + showLocalTime.getHours()
+            : showLocalTime.getHours();
+        let localMins =
+          showLocalTime.getMinutes() < 10
+            ? "0" + showLocalTime.getMinutes()
+            : showLocalTime.getMinutes();
+        return `${localHours} : ${localMins}, ${showLocalTime.getDate()} ${
+          monthNames[showLocalTime.getMonth()]
+        } ${showLocalTime.getFullYear()}`;
+      };
+      localTime.innerHTML = convertTime();
 
       // ---------- details für windberechnung -------------
       if (data.wind.speed < 4.5) {
@@ -48,8 +79,24 @@ form.addEventListener("submit", (e) => {
       cloudiness.innerHTML = data.weather[0].description;
       pressure.innerHTML = data.main.pressure;
       humidity.innerHTML = data.main.humidity;
-      sunrise.innerHTML = data.sys.sunrise + "---- Uhrzeit umrechnen";
-      sunset.innerHTML = data.sys.sunrise + "---- Uhrzeit umrechnen";
+
+      // ---------- convert sunrise/sunset into hour -------------
+
+      let showSuntime = (milliseconds) => {
+        let day, hour, minute, seconds;
+        seconds = Math.floor(milliseconds / 1000);
+        minute = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        hour = Math.floor(minute / 60);
+        minute = minute % 60;
+        day = Math.floor(hour / 24);
+        hour = hour % 24;
+        console.log();
+        return `${hour}:${minute}`;
+      };
+
+      sunrise.innerHTML = showSuntime(data.sys.sunrise);
+      sunset.innerHTML = showSuntime(data.sys.sunset);
       geoCoords.innerHTML = data.coord.lat + "," + data.coord.lon;
 
       console.log(data);
