6b8bd837dd0070581fb3341bf4f16d403e491797
Jennifer Mehren <jennifermehren.jm@gmail.com>
2021-03-07 12:17:24 +0100

    inhalte aus api abgerufen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 // api.openweathermap.org/data/2.5/weather?q=London,uk&appid=78c29c57fa1b0939a568dac3a843325c
 
-
 let button = document.querySelector("button");
 let form = document.querySelector("form");
+
+const cityName = document.querySelector("#cityName");
+const mainIcon = document.querySelector("#mainIcon");
+const temperature = document.querySelector("#temperature");
 
 const localTime = document.querySelector("#localTime .value");
 const wind = document.querySelector("#wind .value");
@@ -13,24 +16,42 @@ const sunrise = document.querySelector("#sunrise .value");
 const sunset = document.querySelector("#sunset .value");
 const geoCoords = document.querySelector("#geoCoords .value");
 
-form.addEventListener("submit",(e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let city = document.querySelector("#searchField");
   city = city.value;
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78c29c57fa1b0939a568dac3a843325c`
-    )
+  )
     .then((response) => response.json())
     .then((data) => {
-      localTime.innerHTML = "Uhrzeit von jetzt";
-      if(data.wind.speed < 4.5){
+      // ---------- main Section ----------------
+      cityName.innerHTML = data.name;
+      mainIcon.innerHTML = "data.weather[0].icon + hier sollte ein Icon sein";
+      temperature.innerHTML = data.main.temp + "in Celsius umrechnen?";
+
+      // ---------- detail Section ----------------
+      localTime.innerHTML =
+        "Welchen Datensatz müssen wir hier nehmen? ---- Uhrzeit umrechnen";
+
+      // ---------- details für windberechnung -------------
+      if (data.wind.speed < 4.5) {
         wind.innerHTML = "easy breasy";
       }
-      if(data.wind.speed < 32){
+      if (data.wind.speed < 32) {
         wind.innerHTML = "windy";
       }
-      if(data.wind.speed > 32){
+      if (data.wind.speed > 32) {
         wind.innerHTML = "stormy";
       }
-      console.log(data)})
-})
+
+      cloudiness.innerHTML = data.weather[0].description;
+      pressure.innerHTML = data.main.pressure;
+      humidity.innerHTML = data.main.humidity;
+      sunrise.innerHTML = data.sys.sunrise + "---- Uhrzeit umrechnen";
+      sunset.innerHTML = data.sys.sunrise + "---- Uhrzeit umrechnen";
+      geoCoords.innerHTML = data.coord.lat + "," + data.coord.lon;
+
+      console.log(data);
+    });
+});
